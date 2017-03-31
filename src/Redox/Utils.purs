e205923df65a3cd0c20d4156193fd57c99ca7182
Marcin Szamotulski <mszamot@gmail.com>
2017-03-31 22:35:26 +0000

    Redox.Utils.hoistCofree'

--- src/Redox/Utils.purs
+++ src/Redox/Utils.purs
@@ -5,6 +5,19 @@ import Control.Comonad.Cofree (Cofree, hoistCofree, (:<), head, tail)
 import Control.Monad.Eff.Unsafe (unsafePerformEff)
 import Redox.Store (Store, setState, performRedoxEff)
 
+-- | A version of `hoistCofree`, where `nat` does not need to come from natural
+-- | transformation.
+-- | This is equivalent of
+-- | [`applyMiddleware`](https://github.com/reactjs/redux/blob/master/src/applyMiddleware.js) 
+-- | from the redux library.
+hoistCofree'
+  :: forall f state
+   . (Functor f)
+  => (f (Cofree f state) -> f (Cofree f state))
+  -> Cofree f state
+  -> Cofree f state
+hoistCofree' nat cf = head cf :< nat (hoistCofree' nat <$> tail cf)
+
 -- | Make interpreter which updates the store on every step of computation.
 -- | You have to supply the store and interperter of type `Cofree f state`.
 -- | Check out test how you can use it.
@@ -14,11 +27,8 @@ mkIncInterp
   => Store state
   -> Cofree f state
   -> Cofree f state
-mkIncInterp store interp = hoist nat interp
+mkIncInterp store interp = hoistCofree' nat interp
   where
-    -- like hoistCofree, but we don't have natural transformation here
-    hoist nat cf = head cf :< nat (hoist nat <$> tail cf)
-
     nat
       :: f (Cofree f state)
       -> f (Cofree f state)
