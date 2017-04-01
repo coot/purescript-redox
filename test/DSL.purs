a0b3b9edb17e78a0410d53b80f2594372f724800
Marcin Szamotulski <mszamot@gmail.com>
2017-04-01 11:29:39 +0000

    test/DSL.purs - add comments, fix imports

--- test/DSL.purs
+++ test/DSL.purs
@@ -11,7 +11,6 @@ import Control.Monad.Eff.Class (liftEff)
 import Control.Monad.Eff.Console (log)
 import Control.Monad.Eff.Unsafe (unsafePerformEff)
 import Control.Monad.Free (Free, liftF)
-import Debug.Trace (trace)
 import Redox.Utils (mkIncInterp)
 import Redox.DSL (dispatch, dispatchP)
 import Redox.Store (REDOX, Store, getState, mkStore, setState)
@@ -20,93 +19,98 @@ import Test.Unit.Assert (assert)
 
 foreign import unsafeLog :: forall a e. a -> Eff e Unit
 
+-- | DSL commands
 data Command a
   = Increment Int a
-  | Decrement Int a
   | IncrementSync Int a
-  | DecrementSync Int a
 
 instance functorCommand :: Functor Command where
   map f (Increment i a) = Increment i (f a)
-  map f (Decrement i a) = Decrement i (f a)
   map f (IncrementSync i a) = IncrementSync i (f a)
-  map f (DecrementSync i a) = DecrementSync i (f a)
 
+-- | This is the type for the abstract syntax tree of our DSL.  `Free` is
+-- | a tree type, and a monad.  The monadic property of it gives a nice way of
+-- | writting programs simply by using `liftF` and writting abstract syntax tree
+-- | directly.  That's nice, since you don't need to write tokenizer, parser
+-- | and all that machinery to get an interpreted language within purescript.
 type DSL a = Free Command a
 
+-- | The following set of function lifts `Command` to the `Free` monad.  This
+-- | is gives API from which you can build complex programs using your the DSL.
+-- | See `cmds` and `cmds2` below
 increment :: Int -> DSL Unit
 increment i = liftF (Increment i unit)
-
-decrement :: Int -> DSL Unit
-decrement i = liftF (Decrement i unit)
 
 incrementSync :: Int -> DSL Unit
 incrementSync i = liftF (IncrementSync i unit)
 
-decrementSync :: Int -> DSL Unit
-decrementSync i = liftF (DecrementSync i unit)
-
+-- | Now we need an interpreter.  We start with a dual functor: `Command` had
+-- | a sum type, hence `Run` is a product.  We use `Aff` since we want to
+-- | interpret the `DSL` in the `Aff` monad.
 newtype Run eff a = Run
   { increment :: Int -> Aff eff a
-  , decrement :: Int -> Aff eff a
   , incrementSync :: Int -> Aff eff a
-  , decrementSync :: Int -> Aff eff a
   }
 
 instance functorRun :: Functor (Run eff) where
-  map f (Run { increment, decrement, incrementSync, decrementSync }) = 
+  map f (Run { increment, incrementSync }) = 
     Run { increment: map f <<< increment
-        , decrement: map f <<< decrement
         , incrementSync: map f <<< incrementSync
-        , decrementSync: map f <<< decrementSync
         }
 
+-- | Basic interpreter.  We only specify how the state will be updated.
+-- | Cofree is a sort of an annotated tree, where each node holds the
+-- | value of current computation (the state), and the branching is done via
+-- | the supplied function, here `Run`.  `Cofree` is an infinite data structure
+-- | so thus it will be able to interpret program of any length written using the
+-- | `Free` monad (or depth)
 type Interp eff a = Cofree (Run eff) a
 
+-- | Use `unfoldCofree` to create the interpreter.
 mkInterp :: forall eff. Int -> Interp eff Int
 mkInterp state = unfoldCofree state id next
   where
     next :: Int -> Run eff Int
     next st = Run { increment: increment st
-                  , decrement: decrement st
                   , incrementSync: incrementSync st
-                  , decrementSync: decrementSync st
                   }
 
     increment :: Int -> Int -> Aff eff Int
     increment a b = later $ pure (a + b)
 
-    decrement :: Int -> Int -> Aff eff Int
-    decrement a b = later $ pure (a - b)
-
     incrementSync :: Int -> Int -> Aff eff Int
     incrementSync a b = pure (a + b)
 
-    decrementSync :: Int -> Int -> Aff eff Int
-    decrementSync a b = pure (a - b)
-
+-- | We need to pair the `Command` and `Run`.  This makes `Run` a dual to
+-- | `Command`.  And you can see why `Run` has to have a product type.
 pair :: forall eff x y. Command (x -> y) -> Run eff x -> Aff eff y
 pair (Increment a next) (Run interp) = next <$> (interp.increment a)
-pair (Decrement a next) (Run interp) = next <$> (interp.decrement a)
 pair (IncrementSync a next) (Run interp) = next <$> (interp.incrementSync a)
-pair (DecrementSync a next) (Run interp) = next <$> (interp.decrementSync a)
 
+-- | This function will be passed to `Redox.dispatch` to interpret `DSL`
+-- | program. You can use `Redox.dispatch` to run programs with this
+-- | interpreter. It will update the store when a DSL program finishes.
 runInterp :: forall eff. DSL (Int -> Int) -> Int -> Aff eff Int
 runInterp cmds state = exploreM pair cmds $ mkInterp state
 
+-- | `runIncInterp` is an enhanced version of `runInterp` which updates the
+-- | store on every step of computation. You can use `Redox.dispatchP` to run
+-- | DSL programs.
 runIncInterp :: forall eff. Store Int -> DSL (Int -> Int) -> Int -> Aff (redox :: REDOX | eff) Int
 runIncInterp store cmds state = exploreM pair cmds $ mkIncInterp store (mkInterp state)
 
+-- | `cmds` is a simple program in our DSL
 cmds :: DSL (Int -> Int)
 cmds = do 
   increment 10
-  decrement 5
+  increment (-5)
   pure id
 
+-- | another DSL program
 cmds2 :: DSL (Int -> Int)
 cmds2 = do
   incrementSync 10
-  decrementSync 5
+  incrementSync (-5)
   pure id
 
 testSuite :: forall eff. TestSuite (redox :: REDOX | eff)
@@ -143,9 +147,6 @@ testSuite =
         cmds2
       state <- liftEff $ getState store
       assert ("store should update " <> show state) (state == 5)
-
-    -- note: if one mixes sync and async commands, they will all run when the
-    -- last async command resolves - due to how Aff bind works.
 
     test "incremental interpreter" $ do
       store <- liftEff $ mkStore 0
