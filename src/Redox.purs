088688c03dc9ceb45580759d02264ee6f0e5d947
Marcin Szamotulski <profunctor@pm.me>
2018-10-07 14:14:56 +0200

    Fixed PureScript-0.12 re-export warnings

--- src/Redox.purs
+++ src/Redox.purs
@@ -4,6 +4,29 @@ module Redox
   , module Utils
   ) where
 
-import Redox.Free as Free
-import Redox.Store as Store
-import Redox.Utils as Utils
+import Redox.Free  (_dispatch
+                   , dispatch
+                   , dispatchP
+                   ) as Free
+import Redox.Store ( Store
+                   , SubscriptionId(..)
+                   , getState
+                   , getSubscriptions
+                   , mkStore
+                   , mkStoreG
+                   , modifyStore
+                   , runStoreSubscriptions
+                   , setState
+                   , subscribe
+                   , unsubscribe
+                   ) as Store
+import Redox.Utils ( addLogger
+                   , addLoggerNat
+                   , compose
+                   , composeM
+                   , hoistCofree'
+                   , mkIncInterp
+                   , mkIncInterpNat
+                   , runSubscriptions
+                   , runSubscriptionsNat
+                   ) as Utils
